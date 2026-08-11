7ec4b806b9328921f28bbebac978295ad92a0816
swapnaamara <swapnaamara59@gmail.com>
2026-08-11 21:01:37 +0530

    Update LRU Cache using OrderedDict.py

--- PYTHON ADVANCE/LRU Cache using OrderedDict.py
+++ PYTHON ADVANCE/LRU Cache using OrderedDict.py
@@ -24,6 +24,6 @@ class LRUCache:
 lru = LRUCache(2)
 lru.put(1, 1)
 lru.put(2, 2)
-print(lru.get(1)) # 1
-lru.put(3, 3) # evicts key 2
-print(lru.get(2)) # -1
+print(lru.get(1)) 
+lru.put(3, 3) 
+print(lru.get(2)) 
