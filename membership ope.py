8cfe845286967ee0ace1d31ac93e80e983c371d3
swapnaamara <swapnaamara59@gmail.com>
2026-08-18 21:50:42 +0530

    Update and rename membershipoperators.py to membership ope.py

--- membership ope.py
+++ membership ope.py
@@ -5,7 +5,6 @@ fruits = ['apple','banana','orange','grape']
 is_apple_in_list = 'apple' in fruits
 print("'apple' is in the list:",is_apple_in_list)
 
-
 #check if watermelon is not in the list
 is_watermelon_not_in_list = 'watermelon' not in fruits
 print("'watermelon' is not in the list:",is_watermelon_not_in_list)
