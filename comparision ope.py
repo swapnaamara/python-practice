4205b44c3cb7e98377f9b204a94296529ffbaf80
swapnaamara <swapnaamara59@gmail.com>
2026-08-18 21:47:21 +0530

    Refactor comparison operations.py
    Removed redundant lines and ensured consistent formatting for comparison operations.

--- comparision ope.py
+++ comparision ope.py
@@ -1,31 +1,25 @@
 a = 10
 b = 20
-
 print(a==b)#equal
 
 a = 10
 b = 20
-
 print(a!=b)#not equal
 
 
 a = 10
 b = 20
-
 print(a<b)
 
 
 a = 10
 b = 20
-
 print(a>b)
 
 a = 10 
 b = 20
-
 print(a>=b)
 
 a = 10 
 b = 10
-
 print(a>=b)
