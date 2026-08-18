410ee236fe6ec514182528de54a13b459fd02268
swapnaamara <swapnaamara59@gmail.com>
2026-08-18 21:53:55 +0530

    Reformat typecasting.py with consistent spacing

--- typecasting.py
+++ typecasting.py
@@ -2,16 +2,13 @@ num = 5+ 2.0
 print(type(num))
 print(num)
 
-
 #type casting to int
 num_int=int(5.7)
 print(num_int)
 
-
 #type casting to float
 num_float=float("3.14")
 print(num_float)
-
 
 #type casting to string
 message = str(42)
