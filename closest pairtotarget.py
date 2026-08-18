1d992d1820e6a17e7ac6ba3eee212f0c1906d920
swapnaamara <swapnaamara59@gmail.com>
2026-08-18 21:43:47 +0530

    Refactor trap_water function implementation
    Refactored the trap_water function for clarity.

--- closest pairtotarget.py
+++ closest pairtotarget.py
@@ -1,10 +1,8 @@
 def trap_water(n, heights):
     if n < 3: 
         return 0  
-        
     left = 0
     right = n - 1
-    
     left_max = heights[left]
     right_max = heights[right]
     total_water = 0
@@ -18,9 +16,7 @@ def trap_water(n, heights):
             right -= 1
             right_max = max(right_max, heights[right])
             total_water += right_max - heights[right]
-            
     return total_water
-
 n = 6
 heights = [3, 0, 0, 2, 0, 4]
 print(trap_water(n, heights))
