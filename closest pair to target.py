47b7f2a1b11a33ba4520497d984c5e02f5600509
swapnaamara <swapnaamara59@gmail.com>
2026-08-19 14:31:07 +0530

    Update and rename closest pairtotarget.py to closest pair to target.py

--- closest pair to target.py
+++ closest pair to target.py
@@ -6,8 +6,10 @@ def trap_water(n, heights):
     left_max = heights[left]
     right_max = heights[right]
     total_water = 0
+    
     while left < right:
         if left_max < right_max:
+            
             left += 1
             left_max = max(left_max, heights[left])
             total_water += left_max - heights[left]
@@ -16,6 +18,7 @@ def trap_water(n, heights):
             right_max = max(right_max, heights[right])
             total_water += right_max - heights[right]
     return total_water
+    
 n = 6
 heights = [3, 0, 0, 2, 0, 4]
 print(trap_water(n, heights))
