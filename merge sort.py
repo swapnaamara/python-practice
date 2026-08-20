aebaada8d19c923bde4f5000b3b74fd873c48f2e
swapnaamara <swapnaamara59@gmail.com>
2026-08-20 21:08:27 +0530

    Refactor merge_arrays function for clarity

--- merge sort.py
+++ merge sort.py
@@ -3,6 +3,7 @@ def merge_arrays(arr1, arr2):
     merged = []
 
     while p1 < len(arr1) and p2 < len(arr2):
+        
         if arr1[p1] < arr2[p2]:
             merged.append(arr1[p1])
             p1 += 1
@@ -11,8 +12,6 @@ def merge_arrays(arr1, arr2):
             p2 += 1
 
     return merged + arr1[p1:] + arr2[p2:]
-
-
 
 a = [1, 3, 5]
 b = [2, 4, 6]
