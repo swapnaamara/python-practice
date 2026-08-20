bdb815dc38cb45949361e3c2f2727e78ab6689b4
swapnaamara <swapnaamara59@gmail.com>
2026-08-20 21:06:38 +0530

    Update and rename asciivalue.py to asciivalues.py

--- asciivalues.py
+++ asciivalues.py
@@ -1,34 +1,17 @@
-'''
 ascii values in com :
 A=65,B=66,C=67
 a=97,b=98,c=9
 
 using ord() and chr()
-'''
-
 
 char_a = 'A'
-
 ascii_a = ord(char_a)
-
 print(ascii_a)
-
-
 
 
 char_b="B"
-
 ascii_b = ord(char_b)
-
 print(ascii_b)
-
-
-
-
 a = 65
-
 ascii_a = chr(a)
-
 print(ascii_a)
-
-
