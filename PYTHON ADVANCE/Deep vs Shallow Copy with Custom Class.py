8ee6971d0593ff2245a813c66daae6e7dfd829d8
swapnaamara <swapnaamara59@gmail.com>
2026-09-18 21:25:43 +0530

    Update Deep vs Shallow Copy with Custom Class.py

--- PYTHON ADVANCE/Deep vs Shallow Copy with Custom Class.py
+++ PYTHON ADVANCE/Deep vs Shallow Copy with Custom Class.py
@@ -6,7 +6,7 @@ class Student:
         self.marks = marks # list
 
 s1 = Student("Ravi", [90, 85])
-s2 = copy.copy(s1) # shallow copy
+s2 = copy.copy(s1) 
 s3 = copy.deepcopy(s1) 
 
 s1.marks[0] = 50
